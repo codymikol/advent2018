358506a41fb4b9a833e1b0c3189b4cbde34d9eae
Cody Mikol <codymikol@gmail.com>
2018-11-30 20:42:44 -0500

    added default test case so I'm ready to go

---
+++ day_01/index.js
@@ -0,0 +1,5 @@
+class Thing {
+    constructor() {
+        this.hello = 'hi'
+    }
+}
---
+++ day_01/test.js
@@ -0,0 +1,11 @@
+describe('A cool example', function () {
+    it('should pass the test', function () {
+        expect(true).toBe(true)
+    });
+
+    it('should work with the index', function () {
+        let test = new Thing();
+        expect(test.hello).toBe('hi')
+    });
+
+});
