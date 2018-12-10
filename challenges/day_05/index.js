a0e58f852073d48449a77c93d930c1361c752da7
Cody Mikol <codymikol@gmail.com>
2018-12-09 20:26:01 -0500

    Optimized algorithm so we don't have to repeat ourselves

--- challenges/day_05/index.js
+++ challenges/day_05/index.js
@@ -2,17 +2,11 @@ export default class Day5 {
 
     static react(input) {
         let ret = input;
-        let solved = false;
 
-        while (!solved) {
-            solved = true;
-            for (let i = 0; i < ret.length - 1; i++) {
-                if(Math.abs(ret.charCodeAt(i) - ret.charCodeAt(i+1)) === 32) {
-                    solved = false;
-                    ret = ret.slice(0, i) + ret.slice(i+2);
-                    break;
-                }
-
+        for (let i = 0; i < ret.length - 1; i++) {
+            if (Math.abs(ret.charCodeAt(i) - ret.charCodeAt(i + 1)) === 32) {
+                ret = ret.slice(0, i) + ret.slice(i + 2);
+                i -= 2;
             }
         }
 
